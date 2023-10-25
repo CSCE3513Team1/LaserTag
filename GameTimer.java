f959add145051c1d3d3406978e9883d3d956c874
Ernesto Sierra <105014176+FleeboG@users.noreply.github.com>
2023-10-25 13:22:05 -0500

    GameTimer.java

--- GameTimer.java
+++ GameTimer.java
@@ -1,28 +1,29 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JPanel;
 
-public class GameTimer {
+
+public class GameTimer extends JPanel{
     private static int secondsRemaining = 30; // Initial countdown value
     private static Timer timer;
     private static JLabel countdownLabel;
-
-
-	public void startCountdown()
-	{
-		JFrame frame = new JFrame("Text Countdown Example");
-        countdownLabel = new JLabel("Countdown: " + secondsRemaining + " seconds");
-
-        // Create a Timer that fires an event every 1000 milliseconds (1 second)
+    
+    public GameTimer()
+    {
+    	
+    	//constructor
+        JLabel label = new JLabel("Countdown: " + secondsRemaining + " seconds");
+     // Create a Timer that fires an event every 1000 milliseconds (1 second)
         timer = new Timer(1000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 secondsRemaining--;
                 if (secondsRemaining >= 0) {
-                    countdownLabel.setText("Countdown: " + secondsRemaining + " seconds");
+                    label.setText("Countdown: " + secondsRemaining + " seconds");
                 } else {
                     // Countdown has reached zero
-                    countdownLabel.setText("Time's up!");
+                    label.setText("Time's up!");
                     timer.stop(); // Stop the timer when countdown reaches zero
                 }
             }
@@ -30,10 +31,12 @@ public class GameTimer {
 
         // Start the timer
         timer.start();
+        add(label);
+    }
 
-		frame.setLocation(50, 50);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(countdownLabel);
-        frame.pack();
-        frame.setVisible(true);
+	public void startCountdown()
+	{
+		
+        countdownLabel = new JLabel("Countdown: " + secondsRemaining + " seconds");
+        
 	}
