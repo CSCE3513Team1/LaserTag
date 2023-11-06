7ab3c00b4aac9fe9e0786781dc95fe2a94cb231d
Ethan Talley <ethan.m.talley@gmail.com>
2023-11-06 16:51:15 -0600

    play action table player update framework

--- PlayActionTable.java
+++ PlayActionTable.java
@@ -9,6 +9,7 @@ public class PlayActionTable {
 
     ArrayList<Player> team1_players;
     ArrayList<Player> team2_players;
+    ArrayList<String> log;
     JFrame frame;
     JPanel panel;
     JPanel panelTwo;
@@ -16,13 +17,16 @@ public class PlayActionTable {
     private GameTimer gameTimer;
     int delay = 1000;
     int swapOut = 0;
+    static int SCORE_INCREMENT = 10;
     
     
     
     PlayActionTable(ArrayList<Player> team1Players, ArrayList<Player> team2Players){
         this.team1_players = new ArrayList<>(team1Players);
         this.team2_players = new ArrayList<>(team2Players);
+        this.log = new ArrayList<>();
     }
+
     public void display(){
         //create jtable
         frame = new JFrame();
@@ -95,6 +99,112 @@ public class PlayActionTable {
             table.setValueAt(score, player + 1, 5);
         }
     }
+
+    //Use this whenever a player hits a player. Make sure to pass in player IDs
+    public void playerHitPlayer(int attacker_id, int defender_id){
+        //update score
+        Player attacker = null;
+        Player defender = null;
+        for(int i = 0; i < team1_players.size(); i++){
+            if(team1_players.get(i).getId() == attacker_id){
+                attacker = team1_players.get(i);
+            }
+            if(team1_players.get(i).getId() == defender_id){
+                defender = team1_players.get(i);
+            }
+        }
+        for(int i = 0; i < team2_players.size(); i++){
+            if(team2_players.get(i).getId() == attacker_id){
+                attacker = team2_players.get(i);
+            }
+            if(team2_players.get(i).getId() == defender_id){
+                defender = team2_players.get(i);
+            }
+        }
+        if (attacker == null || defender == null) {
+            return;
+        }
+        log.add(attacker.getCodename() + " hit " + defender.getCodename());
+        int score = attacker.getScore();
+        score += SCORE_INCREMENT;
+        attacker.setScore(score);
+    }
+
+    //Use this whenever a player hits the base
+    public void playerHitBase(int attacker_id){
+        //unsure how many points this is worth, leaving it empty for now
+        Player attacker = null;
+        for(int i = 0; i < team1_players.size(); i++){
+            if(team1_players.get(i).getId() == attacker_id){
+                attacker = team1_players.get(i);
+            }
+        }
+        for(int i = 0; i < team2_players.size(); i++){
+            if(team2_players.get(i).getId() == attacker_id){
+                attacker = team2_players.get(i);
+            }
+        }
+        attacker.setHitBase(true);
+    }
+
+    public void sortByScore(){
+        this.team1_players.sort((p1, p2) -> p2.getScore() - p1.getScore());
+        this.team2_players.sort((p1, p2) -> p2.getScore() - p1.getScore());
+    }
+
+    //Use this within a clock cycle to update the display
+    public void updateDisplay(){
+        //update table
+        this.sortByScore();
+        String[] columns = {"red_hit", "red_playername", "red_score", "green_hit", "green_playername", "green_score"};
+        String[][] data = new String[team1_players.size() + 1][6];
+        data[0][0] = "";
+        data[0][1] = "Red Team";
+        data[0][2] = "Score";
+        data[0][3] = "";
+        data[0][4] = "Green Team";
+        data[0][5] = "Score";
+        for(int i = 1; i < team1_players.size() + 1; i++){
+            if (team1_players.get(i-1).getHitBase()) {
+                data[i][0] = "B";
+            }
+            else {
+                data[i][0] = "";
+            }
+            data[i][1] = team1_players.get(i-1).getCodename();
+            data[i][2] = String.valueOf(team1_players.get(i-1).getScore());
+        }
+        for(int i = 1; i < team2_players.size() + 1; i++) {
+            if (team2_players.get(i-1).getHitBase()) {
+                data[i][3] = "B";
+            }
+            else {
+                data[i][3] = "";
+            }
+            data[i][4] = team2_players.get(i-1).getCodename();
+            data[i][5] = String.valueOf(team2_players.get(i-1).getScore());
+        }
+        table = new JTable(data, columns);
+        panel.add(table);
+    }
+
     
-    
+    //test
+    /*public static void main(String[] args){
+        ArrayList<Player> team1 = new ArrayList<>();
+        ArrayList<Player> team2 = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            team1.add(new Player(i, "Player " + i));
+        }
+        for (int i = 5; i < 10; i++) {
+            team2.add(new Player(i, "Player " + i));
+        }
+        PlayActionTable playActionTable = new PlayActionTable(team1, team2);
+        playActionTable.display();
+        //test playerHitPlayer
+        playActionTable.playerHitPlayer(4, 7);
+        playActionTable.playerHitBase(2);
+        playActionTable.updateDisplay();
+    }*/
+
 }
--- Player.java
+++ Player.java
@@ -1,17 +1,24 @@
 public class Player {
     private int id;
     private String codename;
+    private int score;
+    boolean hitBase;
 
     Player(int id, String codename){
         this.id = id;
         this.codename = codename;
+        this.score = 0;
     }
 
     public int getId(){ return id; }
     public String getCodename(){ return codename; }
+    public int getScore(){ return score; }
+    public boolean getHitBase(){ return hitBase; }
 
     public void setId(int id){ this.id = id; }
     public void setCodename(String codename){ this.codename = codename; }
+    public void setScore(int score){ this.score = score; }
+    public void setHitBase(boolean hitBase){ this.hitBase = hitBase; }
 
     public String toString(){
         return "Player: " + id + " " + codename;
