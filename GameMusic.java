6a7bbd6943e0dcca9b44f4c8f11db52adbfde840
ZacharyMorganUark <145289212+ZacharyMorganUark@users.noreply.github.com>
2023-11-16 19:37:56 -0600

    Updated GameMusic.java

--- GameMusic.java
+++ GameMusic.java
@@ -1,39 +1,64 @@
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
-import java.util.Random;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Random;
 
 public class GameMusic {
     public static void main(String[] args) {
-        Player player = null;
+        final Player[] player = {null}; // Declare as final array
 
-        //random number between 1-8
-        Random random = new Random();
-        int randomNumber = random.nextInt(8);
-        randomNumber = randomNumber + 1;
+        // random number between 1-8
+        int randomNumber = new Random().nextInt(8) + 1;
 
-        //get the mp3 from github with randomized track number
+        // get the mp3 from github with a randomized track number
         String mp3Source = "https://github.com/CSCE3513Team1/LaserTag/raw/main/Images/Track0" + randomNumber + ".mp3";
 
         try {
             URLConnection urlConnection = new URL(mp3Source).openConnection();
             urlConnection.connect();
             BufferedInputStream bis = new BufferedInputStream(urlConnection.getInputStream());
-            player = new Player(bis);
+            player[0] = new Player(bis);
+
+            // Start a new thread for playback
+            Thread playerThread = new Thread(() -> {
+                try {
+                    player[0].play();
+                } catch (JavaLayerException e) {
+                    System.out.println(e.getMessage());
+                }
+            });
+            playerThread.start();
+
+            // Sleep for 5 seconds
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // Close resources and exit
+            closeResources(player[0], bis);
+            System.exit(0);
+
         } catch (IOException | JavaLayerException e) {
             System.out.println(e.getMessage());
         }
-
-        if (player != null) {
-            try {
-                player.play();
-            } catch (JavaLayerException e) {
-                System.out.println(e.getMessage());
+    }
+    //close buffered input stream and Player
+    private static void closeResources(Player player, BufferedInputStream bis) {
+        try {
+            if (player != null) {
+                player.close();
             }
+            if (bis != null) {
+                bis.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
