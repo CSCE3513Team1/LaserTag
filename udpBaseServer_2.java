48023fe7e2b1d69273f3d7129c1db682fcb7e1c6
Dylan Baker <144173996+dab023@users.noreply.github.com>
2023-11-21 16:15:59 -0600

    Update udpBaseServer_2.java

--- udpBaseServer_2.java
+++ udpBaseServer_2.java
@@ -3,56 +3,61 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.util.ArrayList;
-
-public class udpBaseServer_2
+import java.nio.ByteBuffer;
+import java.util.PriorityQueue;
+import java.util.Queue;
+  
+public class udpBaseServer_2 implements Runnable
 {
-	public static void main(String[] args) throws IOException
+    public static Queue<String> queue = new PriorityQueue<String>();
+    public static udpBaseClient_2 udpBroadcaster;
+
+	public static void baseServer() throws IOException
 	{
 		// Step 1 : Create a socket to listen at port 1234
-		DatagramSocket ds = new DatagramSocket(7500);
-		byte[] receive = new byte[65535];
+        	DatagramSocket ds = new DatagramSocket(7501);
+        	byte[] receive = new byte[1024];
+  
+        	DatagramPacket DpReceive = null;
+        	while (true)
+        	{
+            		// Step 2 : create a DatgramPacket to receive the data.
+            		DpReceive = new DatagramPacket(receive, receive.length);
+  
+            		// Step 3 : receive the data in byte buffer.
+            		ds.receive(DpReceive);
 
-		DatagramPacket DpReceive = null;
-		while (true)
-		{
-
-			// Step 2 : create a DatgramPacket to receive the data.
-			DpReceive = new DatagramPacket(receive, receive.length);
-
-			// Step 3 : receive the data in byte buffer.
-			ds.receive(DpReceive);
-
-			//prints data in the byte buffer.
-			System.out.println("Client:-" + data(receive));
-			
-			// Exit the server if the client sends "bye"
-			if (data(receive).toString().equals("bye"))
-			{
-				System.out.println("Client sent bye.....EXITING");
-				break;
-			}
-
-			// Clear the buffer after every message.
-			receive = new byte[65535];
-		}
+            		//convert to string and add event to queue
+            		String message = new String(DpReceive.getData(), 0, DpReceive.getLength());
+            		queue.add(message);
+            
+            		//Send hit player equipment ID
+            		udpBroadcaster = new udpBaseClient_2(getId(message));
+            		//System.out.println("Client:-" + data(receive));
+  
+            		// Clear the buffer after every message.
+            		receive = new byte[1024];
+        	}
 	}
+   
+    public static String getId(String message)
+    {
+        String[] messageParts = message.trim().split(":");
+        String sendID = messageParts[1];
 
-	// A utility method to convert the byte array
-	// data into a string representation.
-	public static StringBuilder data(byte[] a)
-	{
-		if (a == null)
-			return null;
-		StringBuilder ret = new StringBuilder();
-		int i = 0;
-		while (a[i] != 0)
-		{
-			ret.append((char) a[i]);
-			i++;
-		}
-		return ret;
-	}
+        return sendID;
+    }
+
+    @Override
+    public void run()
+    {
+        try 
+        {
+            baseServer();
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+    }
 }
