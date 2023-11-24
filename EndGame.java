18ec8e280300d306ec918717776dc99b00063fce
nathanreynolds05 <145401064+nathanreynolds05@users.noreply.github.com>
2023-11-24 14:05:40 -0600

    Update EndGame.java

--- EndGame.java
+++ EndGame.java
@@ -1,14 +1,11 @@
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.net.URL;
-import javax.imageio.ImageIO;
-import javax.swing.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.lang.String;
 
 public class EndGame 
 {
@@ -17,12 +14,18 @@ public class EndGame
         JPanel panel = new JPanel();
 
         JButton button = new JButton();
-        button.setText("Click Me");
+        button.setText("End Game");
         button.setBounds(500, 500, 200, 200);
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Button Clicked!");
+                frame.dispose();
+                EntryScreen entryScreen = new EntryScreen();
+		        ArrayList<ArrayList<Player>> teams = entryScreen.waitForPlayerEntry();
+                PlayActionTable playActionTable = new PlayActionTable(teams.get(0), teams.get(1));
+		        playActionTable.display();
+
             }
         });
 
@@ -35,3 +38,6 @@ public class EndGame
     }
 }
 
+//if timer is stopped, then create the button 
+//Have the button wait until the timer is done, and create  a while loop in main that waits for the button to be pressed. 
+//Most likely a copy of the original main code into this while loop
